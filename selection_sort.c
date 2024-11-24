04f05ad4629c70254af7878e791604e830603dc9
chetanpatil84211 <erchetanpatil868@gmail.com>
2024-11-24 23:52:09 +0530

    D_S_A

---
+++ selection_sort.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<limits.h>
+
+int main()
+{
+    int arr[]={5,4,3,2,1};
+    int n=sizeof(arr)/sizeof(arr[0]);
+	printf("size of Array Element is :%d\n",n);
+
+	for(int i=0;i<n;i++)
+	  {
+	    printf("%d\t",arr[i]);
+	   }
+
+	   printf("\n");
+	
+    
+    for(int i=0;i<n-1;i++)
+      {
+          int min=INT_MAX;
+          int miniindex=-1;
+          
+                for(int j=i;j<=n-1;j++)
+                  {
+                      if(min>arr[j])
+                       {
+                           min=arr[j];
+                           miniindex=j;
+                       }
+                  }
+            int temp=arr[miniindex];
+            arr[miniindex]=arr[i];
+            arr[i]=temp;
+			//printf("%d\n",i);
+      }
+      
+      printf("Sorted Array \n");
+      for(int i=0;i<5;i++)
+        {
+            printf("%d\t",arr[i]);
+        }
+
+
+        return 0;
+}
