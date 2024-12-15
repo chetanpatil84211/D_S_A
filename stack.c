42942da27fe6e263120c54f0eb9fb9e7b8e6ae57
chetanpatil84211 <erchetanpatil868@gmail.com>
2024-12-15 22:03:03 +0530

    D_S_A

---
+++ stack.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct stack{
+              int size;
+			  int top;
+			  int *arr;
+			 };
+
+int isEmpty(struct stack *ptr);
+
+int main()
+{
+ 
+ struct stack *s;
+ s->size=20;
+ s->top=-1;
+ s->arr=(int *)malloc(s->size*sizeof(int));
+  
+  //// Adding an element to the stack
+  s->arr[0]=3;
+  s->top++;
+
+ //check stack is Empty or not
+   
+   if(isEmpty(s))
+     {
+	   printf("The stack is Empty\n");
+	 }
+	else
+	 {
+	  printf("The stack is not empty\n");
+	 }
+
+ return 0;
+}
+
+int isEmpty(struct stack *ptr)
+   {
+     if(ptr->top==-1)
+	   {
+	     return 1;
+		}
+	  else
+	   {
+	    return 0;
+	   }
+	}
